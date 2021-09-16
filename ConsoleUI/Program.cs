eb05d4d80b5999d86d4ed6d486ca20e35c836e68
bitan <bitan@DESKTOP-LULIQRU>
2021-09-16 18:59:54 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            var result = productManager.GetAll();
+
+            if (result.Success)
+            {
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine(product.ProductName);
+                }
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+
+            
+
+        }
+    }
+}
